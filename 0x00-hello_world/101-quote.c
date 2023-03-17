e8421fce815b0721c7264c891d724befa06eb138
Daniel Temiagin <talk2drys@gmail.com>
2023-03-16 20:45:09 -0600

    remove backslash

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-const char *data = "and that piece of art is useful\"\
- - Dora Korpar, 2015-10-19\n\0";
+const char *data = "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n\0";
 unsigned long len = strlen(data);
 write(2, data, len);
 return (1);
