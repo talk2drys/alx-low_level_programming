5f9ac32e667fc767bdf8ab16d9c1bbe973f72df9
Daniel Temiagin <talk2drys@gmail.com>
2023-03-16 20:02:26 -0600

    hello world c task done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Return: always zero
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,3 @@
+# Hello World
+project control simple introductory tast to learn the c programm
+language
