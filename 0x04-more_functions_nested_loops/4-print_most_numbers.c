e4e74c73d3dd108553b25f22d2cb6322508e05f2
Daniel Temiagin <talk2drys@gmail.com>
2023-03-24 20:01:56 -0600

    remove braces indentation

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 	for (i = 48; i < 58; i++)
 	{
 		if (i != 50 && i != 52)
-			{
-				_putchar(i + '0');
-			}
+		{
+			_putchar(i + '0');
+		}
 	}
 
 	_putchar('\n');
