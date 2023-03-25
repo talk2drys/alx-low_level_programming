a6c6a656e41a86ed603fab60cdb5de3acfd7b535
Daniel Temiagin <talk2drys@gmail.com>
2023-03-24 20:44:11 -0600

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,9 +17,12 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			if (i != 100) {
+			if (i != 100)
+			{
 				printf("Buzz ");
-			}else {
+			}
+			else
+			{
 				printf("Buzz");
 			}
 		}
@@ -29,9 +32,12 @@ int main(void)
 		}
 		else
 		{
-			if (i != 100) {
+			if (i != 100)
+			{
 				printf("%d ", i);
-			}else {
+			}
+			else
+			{
 				printf("%d", i);
 			}
 		}
