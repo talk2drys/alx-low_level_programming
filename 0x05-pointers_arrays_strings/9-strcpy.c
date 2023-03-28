d35e1f123d01d222b553d0856c3d17215cb9b897
Daniel Temiagin <talk2drys@gmail.com>
2023-03-27 20:34:02 -0600

    fix betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,14 +14,14 @@ char *_strcpy(char *dest, char *src)
 	len = 0;
 
 	while (src[len] != '\0')
-		{
-			len++;
-		}
+	{
+		len++;
+	}
 
 	for (i = 0; i < len; i++)
-		{
-			dest[i] = src[i];
-		}
+	{
+		dest[i] = src[i];
+	}
 	dest[i] = '\0';
 
 	return (dest);
