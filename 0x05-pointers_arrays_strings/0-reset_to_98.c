1002039ac04bf92718897cc4a56fc3a00dfbd99e
Daniel Temiagin <talk2drys@gmail.com>
2023-03-27 19:06:48 -0600

    task 0 done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value store in a pointer of int to 98
+ * @n: pointer to the variable to update
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
