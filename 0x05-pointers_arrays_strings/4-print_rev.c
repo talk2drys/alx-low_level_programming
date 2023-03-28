abcdca88cbdf9aa4725e1d2edf399b41601de632
Daniel Temiagin <talk2drys@gmail.com>
2023-03-27 19:55:21 -0600

    task 4 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "string.h"
+#include <unistd.h>
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	for (; len >= 0; len--)
+	{
+		write(1, &s[len - 1], 1);
+	}
+
+	write(1, &"\n", 1);
+}
