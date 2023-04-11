b45d20de966e44fd73d389cc8ba9e9dcbffb222a
Daniel Temiagin <talk2drys@gmail.com>
2023-04-10 18:05:24 -0600

    task 3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+	/**
+	 * check if we are supplied with 2 argument third is the name of the
+	 * binary
+	 */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+}
