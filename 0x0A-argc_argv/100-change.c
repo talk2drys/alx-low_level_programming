d012e4074e43abd608d8456e8ed84faec783d841
Daniel Temiagin <talk2drys@gmail.com>
2023-04-10 20:42:13 -0600

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,42 +8,33 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
+
 int main(int argc, char *argv[])
 {
-	int cents = 0;
-	int number_coins = 0;
+	int coins[5] = {25, 10, 5, 2, 1};
+	int num_coins = 0, amount, i;
 
-	/**
-	 * we need exactly one argument
-	 */
-	if (argc < 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
+	amount = atoi(argv[1]);
 
-	/**
-	 * we will start dividing from the largest cent to the smallest
-	 * since we are looking for the minimum number of change.
-	 */
-	while (cents > 0)
+	if (amount < 0)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-
-		number_coins += 1;
+		printf("0\n");
+		return (0);
 	}
 
-	printf("%d\n", number_coins);
+	for (i = 0; i < 5; i++)
+	{
+		num_coins += amount / coins[i];
+		amount = amount % coins[i];
+	}
+
+	printf("%d\n", num_coins);
+
 	return (0);
 }
