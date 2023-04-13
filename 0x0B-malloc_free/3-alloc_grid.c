bf3b67fda8c9755a19ecac8924fd28a65273cb90
Daniel Temiagin <talk2drys@gmail.com>
2023-04-12 20:59:42 -0600

    task 3 fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,12 +19,34 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	/* Allocate memory for the 2-dimensional array of integers */
-	grid = (int **)malloc((height * width) * sizeof(int *));
+	/* Allocate memory for the array of pointers to rows */
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 
-	/* Initialize each element 0 */
-	memset(grid, 0, (height * width));
+	/* Allocate memory for each row */
+	for (int i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	/* Initialize each element to 0 */
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
 	return (grid);
 }
