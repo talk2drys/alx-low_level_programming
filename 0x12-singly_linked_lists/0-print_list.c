edd47d96f7baf2dc6df18e81f028d4568176f1d1
Daniel Temiagin <talk2drys@gmail.com>
2023-04-27 20:11:57 -0600

    task done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,106 +1,25 @@
-#include "lists.h"
-#include <stddef.h>
-
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to head of list_t list
- * Return: the number of nodes in the list
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		print_node(h);
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
+		s++;
 	}
 
-	return (count);
-}
-
-/**
- * print_node - prints a single node of a list_t list
- * @node: pointer to node to be printed
- */
-void print_node(const list_t *node)
-{
-	if (node->str == NULL)
-		print_null_node();
-	else
-		print_regular_node(node);
-}
-
-/**
- * print_regular_node - prints a regular node of a list_t list
- * @node: pointer to regular node to be printed
- */
-void print_regular_node(const list_t *node)
-{
-	unsigned int j, i, len = node->len;
-	char str_len[10];
-
-	/* Convert the length to a string */
-	i = 0;
-	while (len > 0)
-	{
-		str_len[i++] = (len % 10) + '0';
-		len /= 10;
-	}
-	str_len[i] = '\0';
-
-	/* Reverse the string */
-	j = 0;
-	i--;
-	while (j < i)
-	{
-		char tmp = str_len[j];
-
-		str_len[j] = str_len[i];
-		str_len[i] = tmp;
-		j++;
-		i--;
-	}
-
-	/* Print the node's data */
-	_putchar('[');
-	for (i = 0; str_len[i] != '\0'; i++)
-		_putchar(str_len[i]);
-	_putchar(']');
-	_putchar(' ');
-
-	for (i = 0; i < node->len; i++)
-		_putchar(node->str[i]);
-	_putchar('\n');
-}
-
-#include "lists.h"
-
-/**
- * print_null_node - prints a null node of a list_t list
- */
-void print_null_node(void)
-{
-	_putchar('[');
-	_putchar('0');
-	_putchar(']');
-	_putchar(' ');
-	print_null_data();
-	_putchar('\n');
-}
-
-/**
- * print_null_data - prints the data for a null node of a list_t list
- */
-void print_null_data(void)
-{
-	_putchar('(');
-	_putchar('n');
-	_putchar('i');
-	_putchar('l');
-	_putchar(')');
+	return (s);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
-int _putchar(char c);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,6 +8,7 @@ int _putchar(char c);
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -20,12 +17,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Function prototypes */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-void print_node(const list_t *node);
-void print_null_node(void);
-void print_regular_node(const list_t *node);
-void print_null_data(void);
-
-#endif /* LISTS_H */
+#endif
