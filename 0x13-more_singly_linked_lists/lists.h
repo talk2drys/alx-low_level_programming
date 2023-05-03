1b31e3d1f3bdfaa92f7cee26c13505ed3779692b
Daniel Temiagin <talk2drys@gmail.com>
2023-05-02 20:43:23 -0600

    update header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
